e56e863b54a76f360aca8bed939d467ba43f5d01
junho <seojh0609@naver.com>
2023-06-13 17:21:01 +0900

    경비 저장, 경비 수정, 이미지 인코딩 및 post요청 기능 추가

---
+++ back/triprecorder/src/main/java/trippers/triprecorder/controller/ExpController.java
@@ -0,0 +1,57 @@
+package trippers.triprecorder.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import trippers.triprecorder.entity.ExpVO;
+import trippers.triprecorder.repository.ExpRepository;
+
+@RestController
+@RequestMapping("/exp")
+public class ExpController {
+
+    @Autowired
+    ExpRepository erepo;
+
+
+    //경비등록
+    @PostMapping("/registerexp")
+    public String ocr(@RequestBody ExpVO exp) {
+        ExpVO savedExp = erepo.save(exp);
+        
+        if (savedExp != null) {
+            return "등록완료";
+        } else {
+            return "등록실패";
+        }
+    }
+    
+    
+    
+    //경비수정
+    @PutMapping("/updateexp/{expNo}")
+    public String updateOcrData(@PathVariable Long expNo, @RequestBody ExpVO exp) {
+
+    	ExpVO existingExp = erepo.findByExpNo(expNo);
+        if (existingExp == null) {
+            return "ExpVO with ID " + expNo + " not found";
+        }
+
+        exp.setExpNo(expNo); 
+        ExpVO updatedExp = erepo.save(exp); 
+        
+        if (updatedExp != null) {
+            return "수정완료";
+        } else {
+            return "수정실패";
+        }
+    }
+    
+    //경비삭제
+    
+}
