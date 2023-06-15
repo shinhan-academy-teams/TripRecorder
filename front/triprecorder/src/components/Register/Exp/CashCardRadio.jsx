319674d625f8d0989ab346300ad896dba3543c70
zzahee <zzahee366@gmail.com>
2023-06-15 17:11:35 +0900

    feat(FE) : 여행 카테고리 등록 완료, sns/경비 등록 페이지 나눔

--- front/triprecorder/src/components/Register/Exp/CashCardRadio.jsx
+++ front/triprecorder/src/components/Register/Exp/CashCardRadio.jsx
@@ -1,11 +1,16 @@
-import { Form, Input, Radio, Select, Space } from "antd";
+import { Form, Radio, Select, Space } from "antd";
 import { useRecoilState } from "recoil";
-import { cardShow, payMethodAtom } from "recoil/RegisterExpAtom";
+import { cardShowAtom, payMethodAtom, showAtom } from "recoil/RegisterExpAtom";
 
 const CashCardRadio = () => {
   const [value, setValue] = useRecoilState(payMethodAtom);
-  const [show, setShow] = useRecoilState(cardShow);
+  const [show, setShow] = useRecoilState(showAtom);
+  const [showCard, setShowCard] = useRecoilState(cardShowAtom);
 
+  const selectCard = (cards) => {
+    console.log(cards);
+    setShowCard(cards);
+  };
   return (
     <>
       <Form.Item
@@ -29,20 +34,74 @@ const CashCardRadio = () => {
           </Space>
 
           {show && (
-            <div id="divshow">
+            <div id="divshow" style={{ display: "block" }}>
               <Form.Item
                 label="카드사"
                 name="cardcompany"
                 rules={[
                   {
                     required: true,
-                    message: "카드사를 입력해주세요!",
+                    message: "카드사를 골라주세요!",
                   },
                 ]}
+                onChange={selectCard}
               >
-                <Input />
+                <Select
+                  showSearch
+                  style={{
+                    width: 200,
+                  }}
+                  placeholder="선택 안함"
+                  optionFilterProp="children"
+                  // filterOption={(input, option) =>
+                  //   (option?.label ?? "").includes(input)
+                  // }
+                  // filterSort={(optionA, optionB) =>
+                  //   (optionA?.label ?? "")
+                  //     .toLowerCase()
+                  //     .localeCompare((optionB?.label ?? "").toLowerCase())
+                  // }
+                  value={selectCard}
+                  options={[
+                    {
+                      value: "shinhan",
+                      label: "신한",
+                    },
+                    {
+                      value: "kb",
+                      label: "국민",
+                    },
+                    {
+                      value: "nh",
+                      label: "농협",
+                    },
+                    {
+                      value: "hana",
+                      label: "하나",
+                    },
+                    {
+                      value: "woori",
+                      label: "우리",
+                    },
+                    {
+                      value: "ibk",
+                      label: "기업",
+                    },
+                    {
+                      value: "samsung",
+                      label: "삼성",
+                    },
+                    {
+                      value: "hd",
+                      label: "현대",
+                    },
+                    {
+                      value: "lotte",
+                      label: "롯데",
+                    },
+                  ]}
+                />
               </Form.Item>
-
               <Form.Item
                 label="결제 카드"
                 name="card"
@@ -53,11 +112,15 @@ const CashCardRadio = () => {
                   },
                 ]}
               >
-                <Select>
-                  <Select.Option value="a">a카드</Select.Option>
-                  <Select.Option value="b">b카드</Select.Option>
-                  <Select.Option value="c">c카드</Select.Option>
-                </Select>
+                {showCard.length > 0 ? (
+                  <Select>
+                    <Select.Option value="a">a카드</Select.Option>
+                    <Select.Option value="b">b카드</Select.Option>
+                    <Select.Option value="c">c카드</Select.Option>
+                  </Select>
+                ) : (
+                  <Select></Select>
+                )}
               </Form.Item>
             </div>
           )}
