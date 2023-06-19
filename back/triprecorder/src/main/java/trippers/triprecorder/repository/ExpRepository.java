b1c07f9c3b817f2abc1b2aca39e1ae06df343249
junho <seojh0609@naver.com>
2023-06-19 09:37:05 +0900

    (BE) : 상위 3 카드 조회 및 혜택 조회 api 구현

--- back/triprecorder/src/main/java/trippers/triprecorder/repository/ExpRepository.java
+++ back/triprecorder/src/main/java/trippers/triprecorder/repository/ExpRepository.java
@@ -3,6 +3,8 @@ package trippers.triprecorder.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import trippers.triprecorder.entity.ExpVO;
@@ -11,5 +13,10 @@ import trippers.triprecorder.entity.TripVO;
 @Repository
 public interface ExpRepository extends JpaRepository<ExpVO, Long> {
 	public ExpVO findByExpNo(Long expNo);
+
 	List<ExpVO> findByTripAndSnsNull(TripVO trip);
+
+	@Query("SELECT e.card, COUNT(*) AS usageCount " + "FROM ExpVO e " + "Where e.exp_cate= :category"
+			+ "GROUP BY e.card " + "ORDER BY usageCount DESC")
+	List<ExpVO> findTop3CardNumbersByExpCate(String category);
 }
