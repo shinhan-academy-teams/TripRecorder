6004dad56f4f3434b64fbb984698a2f73cdea357
junho <seojh0609@naver.com>
2023-06-22 09:12:44 +0900

    (BE) : 프로필 수정완료

--- back/triprecorder/src/main/java/trippers/triprecorder/repository/DiscountRepository.java
+++ back/triprecorder/src/main/java/trippers/triprecorder/repository/DiscountRepository.java
@@ -8,6 +8,7 @@ import trippers.triprecorder.entity.CardVO;
 import trippers.triprecorder.entity.DiscountVO;
 
 public interface DiscountRepository extends JpaRepository<DiscountVO, Long> {
+	//카테고리와 카드번호를 가지고 할인혜택조회 메서드
     List<DiscountVO> findByDcCateAndCardIn(String category, List<CardVO> cards);
 }
 
