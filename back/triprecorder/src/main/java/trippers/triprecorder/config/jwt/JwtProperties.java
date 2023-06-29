900dbb06db16d76402afe12daf8b90de2d261dd1
zzahee <zzahee366@gmail.com>
2023-06-29 19:19:13 +0900

    clean main branch

--- back/triprecorder/src/main/java/trippers/triprecorder/config/jwt/JwtProperties.java
+++
@@ -1,8 +0,0 @@
-package trippers.triprecorder.config.jwt;
-
-public class JwtProperties {
-	static final String SECRET = "TripRecorderByTrippers";
-	static final int EXPIRATION_TIME = 1000 * 60 * 60 * 24; // 일단 하루
-	static final String TOKEN_PREFIX = "Bearer ";
-	static final String HEADER_STRING = "Authorization";
-}
--- back/triprecorder/src/main/java/trippers/triprecorder/dto/LoginRequestDto.java
+++
@@ -1,9 +0,0 @@
-package trippers.triprecorder.dto;
-
-import lombok.Data;
-
-@Data
-public class LoginRequestDto {
-	private String userId;
-	private String userPw;
-}
